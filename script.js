775d4d936864baabd27469ccb32a7436d8bb9340
Uhaimong <uhaimarma2016@gmail.com>
2025-07-25 10:48:10 +0600

    bmi calculator

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Toggle menu on icon click
   menuIcon.addEventListener("click", function (event) {
-    event.stopPropagation(); // Prevent bubbling
+    event.stopPropagation();
     navList.style.marginLeft =
       navList.style.marginLeft === "0px" ? "-9999999px" : "0px";
   });
@@ -21,4 +21,54 @@ document.addEventListener("DOMContentLoaded", function () {
       navList.style.marginLeft = "-9999999px";
     });
   });
+  // Active nav li
+  const navliList = document.querySelectorAll("li");
+  navliList.forEach((item) => {
+    item.addEventListener("click", function () {
+      // remove previous defalut active class
+      navliList.forEach((nav) => nav.classList.remove("active-nav"));
+      // add new active class
+      item.classList.add("active-nav");
+    });
+  });
+});
+
+let bmiStatus = "";
+let health = "";
+const bmiValue = document.getElementById("bmiValue");
+const bmiStatusText = document.getElementById("bmiStatus");
+const healthText = document.getElementById("health");
+const submit = document.getElementById("submitButton");
+submit.addEventListener("click", function (event) {
+  event.preventDefault();
+  const height = document.getElementById("height").value;
+  const weight = document.getElementById("weight").value;
+  const age = document.getElementById("age").value;
+  const sex = document.getElementById("sex").value;
+  const heightInMeter = height / 100;
+  const bmi = parseFloat((weight / (heightInMeter * heightInMeter)).toFixed(2));
+
+  if (bmi <= 18.5) {
+    health =
+      "Increase calorie intake with nutritious foods; consult a doctor for possible deficiencies.";
+    bmiStatus = "Underweight";
+  }
+  if (bmi > 18.5 && bmi <= 24.9) {
+    health = "Maintain a balanced diet and regular exercise.";
+    bmiStatus = "Healthy";
+  }
+  if (bmi >= 25 && bmi <= 29.9) {
+    health =
+      "Focus on portion control, regular physical activity, and reducing processed foods.";
+    bmiStatus = "Overweight";
+  }
+  if (bmi >= 30) {
+    health =
+      "Adopt a weight loss plan with medical guidance; prioritize whole foods and regular workouts.";
+    bmiStatus = "Obesity";
+  }
+  bmiValue.innerHTML = bmi;
+  bmiStatusText.innerHTML = bmiStatus;
+  healthText.innerHTML = health;
+  console.log("status: ", bmiStatus, "Health: ", health, "BMI: ", bmi);
 });
